81afa6dc9a35bf46dc1d1fdf222cc3911591006e
ML <emele18@gmail.com>
2013-05-20 14:20:30 -0430

    Creacion de la app AskBeaver

---
+++ AskBeaver/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AskBeaver::Application.config.secret_token = 'c7def7957dea51d2812be03e3bbcb6e1749f016e666ec6a851764c8f47198d220f05e41b0183507ee25b1eb316e228f47a2ee210423cb905861ec0556416093d'
